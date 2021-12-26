6ef054eee4ffd61c35fa5acd7c9c1bc82df263d5
nampt <nampt@nampts-Mac-mini.local>
2021-12-26 11:18:48 +0700

    feat: 🎸 render streamer list

--- public/javascripts/axios.js
+++ public/javascripts/axios.js
@@ -1,12 +1,46 @@
-const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-let dataset = [];
 
-const promises = channels.map(async (channel) => {
-  const { data } = await axios.get(`https://twitch-proxy.freecodecamp.rocks/twitch-api/channels/${channel}`);
-  return data;
-});
+// BOM
+window.addEventListener('load', async () => {
+  const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  let dataset = [];
 
-dataset = await Promise.all(promises);
+  const promises = channels.map(async (channel) => {
+    const { data } = await axios.get(`https://twitch-proxy.freecodecamp.rocks/twitch-api/channels/${channel}`);
+    return data;
+  });
 
-console.log("🚀 ~ file: axios.js ~ line 9 ~ dataset", dataset);
+  dataset = await Promise.all(promises);
+
+  console.log("🚀 ~ file: axios.js ~ line 9 ~ dataset", dataset);
+
+  const streamers = document.querySelector('#streamers');
+
+  
+
+  dataset.forEach(item => {
+    // Render streamer list
+    const streamerItem = document.createElement('li');
+
+    // Render streamer's logo
+    const imgEle = document.createElement('img'); // src
+    imgEle.src = item.logo;
+
+    // Render streamer's link
+    const linkEle = document.createElement('a'); // href
+    linkEle.innerHTML = item.display_name;
+    linkEle.href = item.url;
+
+    // Render streamer's status
+    const statusEle = document.createElement('span');
+    statusEle.innerHTML = `${item.name}: ${item.status}`;
+
+    streamerItem.appendChild(imgEle);
+    streamerItem.appendChild(linkEle);
+    streamerItem.appendChild(statusEle);
+
+    streamers.appendChild(streamerItem);
+  })
+})
+
+
 
--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,8 @@ const axios = require('axios').default;
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  const channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-  let dataset = [];
-  
-
-  try {
-    // const promises = channels.map(async (channel) => {
-    //   const { data } = await axios.get(`https://twitch-proxy.freecodecamp.rocks/twitch-api/channels/${channel}`);
-    //   return data;
-    // })
-    // console.log("🚀 ~ file: index.js ~ line 16 ~ promises ~ promises", promises)
-      
-
-    // dataset = await Promise.all(promises);
-
-    // for (const channel of channels) {
-    //   const { data } = await axios.get(`https://twitch-proxy.freecodecamp.rocks/twitch-api/channels/${channel}`);
-    //   dataset.push(data);
-    // }
-
-    
+router.get('/', async function(req, res, next) {   
     res.render('index', {title: 'Hello mn'});
-    // res.send(dataset);
-  } catch (error) {
-    console.log(error)
-  }   
 });
 
 router.get('/testapi', (req, res) => {
